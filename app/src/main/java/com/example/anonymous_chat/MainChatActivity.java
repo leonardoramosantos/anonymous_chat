65888df571c2a2e86539fe438107d383419a2bd1
Leonardo Ramos dos Santos <leonardo.ramosantos@yahoo.com.br>
2023-05-05 19:36:00 -0300

    Splash Screen and first activity

---
+++ app/src/main/java/com/example/anonymous_chat/MainChatActivity.java
@@ -0,0 +1,14 @@
+package com.example.anonymous_chat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_chat);
+    }
+}
---
+++ app/src/main/java/com/example/anonymous_chat/SplashScreenActivity.java
@@ -0,0 +1,17 @@
+package com.example.anonymous_chat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        startActivity(new Intent(this, MainChatActivity.class));
+        finish();
+    }
+}
