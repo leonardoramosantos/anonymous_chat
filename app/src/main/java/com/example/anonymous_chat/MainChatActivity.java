acaaa050e85d98faad45d47197c7e925d1a27743
Leonardo Ramos dos Santos <leonardo.ramosantos@yahoo.com.br>
2023-05-10 20:17:17 -0300

    menu "sobre o sistema"

--- app/src/main/java/com/example/anonymous_chat/MainChatActivity.java
+++ app/src/main/java/com/example/anonymous_chat/MainChatActivity.java
@@ -2,7 +2,11 @@ package com.example.anonymous_chat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainChatActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class MainChatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_chat);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_chat_menu_options, menu);
+
+        return true;
+    }
+
+    public void onAboutSystemClick(MenuItem menu) {
+        Intent aboutSystemIntent = new Intent(this, AboutSystemActivity.class);
+        startActivity(aboutSystemIntent);
     }
 }
